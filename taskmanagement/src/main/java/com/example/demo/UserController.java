b86486c7658654a275eb8fdd1df7c4315bd8b6c9
Prashant Annasaheb Shinde <124038708+prashant541@users.noreply.github.com>
2024-08-25 21:48:21 +0530

    Update UserController.java

--- taskmanagement/src/main/java/com/example/demo/UserController.java
+++ taskmanagement/src/main/java/com/example/demo/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api")
 public class UserController {
     @Autowired
     private UserService userService;
